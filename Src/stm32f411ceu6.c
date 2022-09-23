96680a8bc16d493c79b609c3baf3f042700b1a3c
washburn961 <gabribortto@gmail.com>
2022-09-22 22:21:56 -0300

    Added authors

---
+++ Src/stm32f411ceu6.c
@@ -0,0 +1,14 @@
+/**
+ ******************************************************************************
+ * @file           : stm32f411ceu6.c
+ * @author         : Gabriel Bortoletto Molz
+ * @brief          : Low level functions
+ ******************************************************************************
+ * @attention
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
