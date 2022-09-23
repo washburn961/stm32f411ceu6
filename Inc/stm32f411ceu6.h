b954eda985c2221e6e2a3bc03c869d27f7c7f7dc
washburn961 <gabribortto@gmail.com>
2022-09-22 22:22:03 -0300

    Added authors

--- Inc/stm32f411ceu6.h
+++ Inc/stm32f411ceu6.h
@@ -1,3 +1,17 @@
+/**
+ ******************************************************************************
+ * @file           : stm32f411ceu6.h
+ * @author         : Gabriel Bortoletto Molz
+ * @brief          : Declarations for low level functions
+ ******************************************************************************
+ * @attention
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 
 #include <stdint.h>
 #include <stdlib.h>
